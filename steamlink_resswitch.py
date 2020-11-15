4828d308ea464a2524f512b46786d302af4a9973
Alex Corrie <ajccode@gmail.com>
2020-11-15 17:38:12 +0000

    Add note to resolution switcher

--- steamlink_resswitch.py
+++ steamlink_resswitch.py
@@ -1,4 +1,14 @@
-"""Toggle Resolution."""
+"""Toggle desktop resolution."""
+
+# This is a quick hack - the specific registry key will differ as I
+# found out later when this stopped working. Obviously the resolution
+# switching between UW and HD is also ultra specific. The reason for
+# the hack is to implement it as a Steam executable "game" so that I
+# can toggle desktop resolution via the Steam Link - some games don't
+# play nice with the host res differing from the target monitor res.
+# Basically, they are a bit too clever about it and fall over,
+# screwing up aspect ratio etc.
+
 
 import os
 import winreg
@@ -11,7 +21,7 @@ APP_INSTALL = os.path.join(os.environ['USERPROFILE'], 'apps',
 def get_xresolution():
     key = winreg.OpenKey(
         winreg.HKEY_LOCAL_MACHINE,
-        (r'SYSTEM\CurrentControlSet\Hardware Profiles\UnitedVideo\CONTROL\VIDEO\{48150032-5642-401B-97CF-5C845BFF31AE}\0000'),
+        (r'SYSTEM\CurrentControlSet\Hardware Profiles\UnitedVideo\CONTROL\VIDEO\{6CF520FF-2974-11E8-9E3A-C32D69779901}\0000'),
         winreg.KEY_QUERY_VALUE
     )
 
