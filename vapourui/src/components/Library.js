a8f5269f21c32d81b1a78165d7c0bd753802b241
Alex Corrie <ajccode@gmail.com>
2020-11-19 01:09:57 +0000

    Add an explicit library component with disk space

---
+++ vapourui/src/components/Library.js
@@ -0,0 +1,18 @@
+import logo from '../logo.svg'
+import Accordion from './Accordion'
+
+
+export default function Library(props) {
+    return (
+      <Accordion title={props.path}>
+        <img src={logo} className="App-logo" alt="logo" />
+
+        <p>{humanise(props.size)} | {humanise(props.free)} free</p>
+      </Accordion>
+    )
+}
+
+function humanise(size) {
+    var i = Math.floor( Math.log(size) / Math.log(1024) );
+    return ( size / Math.pow(1024, i) ).toFixed(2) * 1 + ' ' + ['B', 'kiB', 'MiB', 'GiB', 'TiB'][i];
+};
--- vapourui/src/components/LibrarySet.js
+++ vapourui/src/components/LibrarySet.js
@@ -1,6 +1,5 @@
 import { useState, useEffect } from 'react';
-import logo from '../logo.svg'
-import Accordion from './Accordion'
+import Library from './Library'
 
 
 const endpoint = 'http://127.0.0.1:8000/libraries/'
@@ -23,11 +22,7 @@ export default function LibrarySet (props) {
 
       for (let library of libraries) {
         final.push(
-          <Accordion title={library.path}>
-            <img src={logo} className="App-logo" alt="logo" />
-
-             Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-          </Accordion>
+          <Library path={library.path} size={library.size} free={library.freeBytes}/>
         )
       }
 
