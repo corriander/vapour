878025ca1c21cdc5bc51b7f11bed6ff4d49bfe99
Alex Corrie <ajccode@gmail.com>
2020-11-24 21:40:58 +0000

    Update API models to reflect new games property
    API models were still being built from a dict

--- vapour/api/main.py
+++ vapour/api/main.py
@@ -39,12 +39,12 @@ def read_archives():
 
 @app.get("/libraries/{library_id}/games/")
 def read_library_games(library_id: int):
-    return [Game(**dict(game)) for game in steam.libs[library_id].games.values()]
+    return [Game(**dict(game)) for game in steam.libs[library_id].games]
 
 @app.get("/games/")
 def read_all_games():
-    return [Game(**dict(game)) for lib in steam.libs for game in lib.games.values()]
+    return [Game(**dict(game)) for lib in steam.libs for game in lib.games]
 
 @app.get("/games/{game_id}", response_model=Game)
 def read_game(game_id: int):
-    return {game.id: game for game in [Game(**dict(game)) for lib in steam.libs for game in lib.games.values()]}[game_id]
+    return {game.id: game for game in [Game(**dict(game)) for lib in steam.libs for game in lib.games]}[game_id]
