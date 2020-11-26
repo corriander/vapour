e9c225d116d1d32e1d85f488d683ae2c54fbac65
Alex Corrie <ajccode@gmail.com>
2020-11-26 00:43:37 +0000

    Game sizes in GiB and proper use of Memo for table

--- vapourui/src/components/Library.js
+++ vapourui/src/components/Library.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import Accordion from './Accordion'
 import LibraryChart from './LibraryChart'
 import LibraryTable from './LibraryTable'
@@ -7,24 +8,35 @@ import { getGames } from '../services/libraryService';
 
 
 export default function Library(props) {
-    const { data: games, isLoading, error } = useQuery(["librarygames", props.id], getGames);
+    const { data: games, isLoading } = useQuery(["librarygames", props.id], getGames);
 
     // Prepare data for components
+    const gebify = x => (x / Math.pow(1024, 3)).toFixed(2);
     const threshold = 10e9 / (props.size + props.free) * 100;
-    const columns = [
-      {
-        Header: "Game",
-        accessor: "name",
-      },
-      {
-        Header: "Size [B]",
-        accessor: "size",
-      },
-    ]
+    // THANK YOU https://www.reddit.com/r/reactjs/comments/jjz93y/how_do_i_use_reactquery_to_join_data_from/
+    // This was driving me mad - would only work on a hot update without the conditional in there to prevent
+    // a compile failure because games doesn't exist when we try to .map
+    const humanisedGames = React.useMemo(() => {
+      return isLoading ? [] : games.map(obj => ({ ...obj, size: gebify(obj.size) }))
+    }, [games]);
+
+    const columns = React.useMemo(() => [
+        {
+          Header: "Game",
+          accessor: "name",
+          isNumeric: false
+        },
+        {
+          Header: "Size (GiB)",
+          accessor: "size",
+          isNumeric: true
+        },
+      ]
+    )
     return (
       <Accordion title={props.path}>
-        <LibraryChart id={props.id} free={props.free} threshold={threshold} data={games}/>
-        <LibraryTable libraryId={props.id} columns={columns} data={games}/>
+        <LibraryChart id={props.id} free={gebify(props.free)} threshold={threshold} data={humanisedGames}/>
+        <LibraryTable libraryId={props.id} columns={columns} data={humanisedGames}/>
       </Accordion>
     )
 }
