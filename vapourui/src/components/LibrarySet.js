e9b8875393643cc83b84a16d953be90978aa8e29
Alex Corrie <ajccode@gmail.com>
2020-12-07 14:27:44 +0000

    Introduce some crude error handling for archives
    Probably needs addressing in the backend, but WSL is being
    a bit weird about filesystems and sometimes the archives don't
    get accessed properly first time / without a restart

--- vapourui/src/components/LibrarySet.js
+++ vapourui/src/components/LibrarySet.js
@@ -9,25 +9,28 @@ export default function LibrarySet (props) {
       const { data: libraries, isLoading: librariesLoading, error: librariesError } = useQuery("libraries", getLibraries);
       const { data: archives, isLoading: archivesLoading, error: archivesError } = useQuery("archives", getArchives);
 
-      const archiveElements = [];
-      const libraryElements = [];
-
       const final = [];
 
       if (librariesLoading || archivesLoading) return "Loading...";
-      if (archivesError) return "Error loading archives"
-      if (librariesError) return "Error loading libraries"
 
-      for (let library of libraries) {
-        final.push(
-          <Library id={library.id} path={library.path} size={library.size} free={library.freeBytes}/>
-        )
+      if (!librariesError) {
+        for (let library of libraries) {
+          final.push(
+            <Library id={library.id} path={library.path} size={library.size} free={library.freeBytes}/>
+          )
+        }
+      } else {
+        console.error("Error loading libraries")
       }
 
-      for (let archive of archives) {
-        final.push(
-          <Archive id={archive.id} path={archive.path} size={archive.size} free={archive.freeBytes} maxSize={archive.maxSize}/>
-        )
+      if (!archivesError) {
+        for (let archive of archives) {
+          final.push(
+            <Archive id={archive.id} path={archive.path} size={archive.size} free={archive.freeBytes} maxSize={archive.maxSize}/>
+          )
+        }
+      } else {
+        console.error("Error loading archives")
       }
 
       return (
